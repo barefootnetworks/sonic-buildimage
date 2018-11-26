37f63c9ba8224b4a4cd18f2e01c75b21508be035
Nadiya.Stetskovych <nstetskovych@barefootnetworks.com>
2018-11-26 08:03:21 -0800

    Small formating

--- platform/barefoot/bfn-platform.mk
+++ platform/barefoot/bfn-platform.mk
@@ -4,7 +4,7 @@ ifeq ($(CONFIGURED_P4_STACK),p4_16)
 $(BFN_PLATFORM)_PATH = /sonic # use local path, should be changed as soon as we released p4-16 and make pub available
 SONIC_COPY_DEBS +=$(BFN_PLATFORM)
 else
-$(BFN_PLATFORM)_URL =   "https://github.com/barefootnetworks/sonic-release-pkgs/raw/sde-sai1.3.3/bfnplatform_1.0.0_amd64.deb"
+$(BFN_PLATFORM)_URL = "https://github.com/barefootnetworks/sonic-release-pkgs/raw/sde-sai1.3.3/bfnplatform_1.0.0_amd64.deb"
 SONIC_ONLINE_DEBS += $(BFN_PLATFORM) # $(BFN_SAI_DEV)
 endif
 
